9537c20c8af8c3f2f30cb8717c4b222aad37dfd4
Debadyuti Mukherjee <dm3400@srmist.edu.in>
2026-02-11 16:41:52 +0530

    CORS fix

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="Online Proctoring Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://proctorfe.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
